41751d1a856389a2c3a36df9ce7855287a470236
Dima Telechkun <telechkun93@gmail.com>
2023-01-13 13:45:33 +0200

    final corrections were applied

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -2,7 +2,7 @@ import css from './Button.module.css';
 
 export const Button = ({ onLoadMore }) => {
   return (
-    <div>
+    <div className={css.btn}>
       <button type="button" className={css.Button} onClick={onLoadMore}>
         Load more
       </button>
